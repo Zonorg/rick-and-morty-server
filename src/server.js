ef81a560d3dcba468ef2489ae415c3691123bd60
Renzo Viscio <renzo@gecco.com.mx>
2024-05-05 22:25:02 -0300

    dotenv

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const server = express();
 const cors = require("cors");
 const morgan = require("morgan");
-const PORT = 3001;
+const PORT = process.env.PORT;
 const router = require("./routes/index");
 const favsRouter = require("./routes/favsRouter");
 const { sequelize } = require("./database/index");
 
-server.use(express.json()); 
-server.use(cors()); 
+server.use(express.json());
+server.use(cors());
 server.use(morgan("dev"));
 server.use("/rickandmorty", router);
 server.use("/favs", favsRouter);
